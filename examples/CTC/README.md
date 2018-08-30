20bb30044f80ccb4cc1ad50e5e4296bf81f2bec3
eastonYi <yicheng1994@outlook.com>
2018-08-30 15:34:46 +0800

    test

--- examples/CTC/README.md
+++
@@ -1,3 +0,0 @@
-参考: https://distill.pub/2017/ctc/
-
-只有同label(非blank)extend时才有可能出现分支
--- scp.sh
+++
@@ -1,4 +0,0 @@
-scp -r /Users/easton/Projects/eastonCode/ xushuang2@172.18.36.31:~/easton/
-scp -r /Users/easton/Projects/eastonCode/ xushuang@172.18.36.82:~/easton/
-scp -r /Users/easton/Projects/eastonCode/ sxu@172.18.30.90:~/easton/
-scp -r /Users/easton/Projects/eastonCode/ xwzhang@172.18.30.104:~/easton/
